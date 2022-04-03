5db14ca9a0a0f9bfe91b9c2e57ec67daabd43b02
Hyeonjiwon <zlone1208@gmail.com>
2022-04-03 19:24:11 +0900

    JPA를 활용해 데이터 저장하기

--- firstProject/src/main/java/com/example/firstProject/controller/ArticleController.java
+++ firstProject/src/main/java/com/example/firstProject/controller/ArticleController.java
@@ -1,12 +1,18 @@
 package com.example.firstProject.controller;
 
 import com.example.firstProject.dto.ArticleForm;
+import com.example.firstProject.entity.Article;
+import com.example.firstProject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    @Autowired // spring boot가 미리 생성해놓은 객체를 가져다가 자동 연결
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -17,6 +23,15 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+
+        // 1. Dto를 Entity로 변환
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        // 2. Repository에게 Entity를 DB 안에 저장하게 함
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+
         return "";
     }
 }
