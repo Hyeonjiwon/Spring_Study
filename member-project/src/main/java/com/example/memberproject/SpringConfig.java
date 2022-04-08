41c26cb8516c98239fb32c68c00ec666e06e8e5f
Hyeonjiwon <zlone1208@gmail.com>
2022-04-08 19:35:42 +0900

    자바 코드로 직접 스프링 빈 등록하기

--- member-project/src/main/java/com/example/memberproject/SpringConfig.java
+++ member-project/src/main/java/com/example/memberproject/SpringConfig.java
@@ -1,2 +1,21 @@
-package com.example.memberproject;public class SpringConfig {
+package com.example.memberproject;
+
+import com.example.memberproject.repository.MemberRepository;
+import com.example.memberproject.repository.MemoryMemberRepository;
+import com.example.memberproject.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
--- member-project/src/main/java/com/example/memberproject/service/MemberService.java
+++ member-project/src/main/java/com/example/memberproject/service/MemberService.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+// @Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // 직접 생성하는 것이 아니라 외부에서 넣어주도록
-    @Autowired
+    // @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
