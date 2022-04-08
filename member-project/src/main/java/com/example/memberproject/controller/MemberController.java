c241aa71fd457cfbe3d8423e5c8a63639cd7200a
Hyeonjiwon <zlone1208@gmail.com>
2022-04-08 21:24:33 +0900

    회원 조회

--- member-project/src/main/java/com/example/memberproject/controller/MemberController.java
+++ member-project/src/main/java/com/example/memberproject/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.example.memberproject.domain.Member;
 import com.example.memberproject.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,5 +33,12 @@ public class MemberController {
 
         memberService.join(member); // 회원가입 할 때 만든 join
         return "redirect:/"; // 회원가입 후 다시 home으로
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers(); // 모든 멤버 꺼내기
+        model.addAttribute("members", members); // 멤버 리스트를 모델에 담기
+        return "members/memberList";
     }
 }
